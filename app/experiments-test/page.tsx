3fb8436c50769db6782321ffa92fc2f43045e4d0
guidogpp <guidogpp@inditex.com>
2026-01-16 01:49:36 +0100

    fix: forzar experiments-test a dinámica para evitar prerender

--- app/experiments-test/page.tsx
+++ app/experiments-test/page.tsx
@@ -1,5 +1,7 @@
 import ExperimentsTestClient from '@/components/experiments/ExperimentsTestClient'
 
+export const dynamic = 'force-dynamic'
+
 export default function ExperimentsTestPage() {
   return <ExperimentsTestClient />
 }
